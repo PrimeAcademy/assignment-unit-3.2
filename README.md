fce76107c3de37cdc2c99c8f228eefba75c9beab
mbmosman <mbMosman@users.noreply.github.com>
2021-03-05 18:04:28 -0600

    initial split

---
+++ README.md
@@ -0,0 +1,36 @@
+# Prime Tier 1 Week 3 - Part 2
+
+This week, we will continue to put more into your tool belt by tackling methods for handling tasks multiple times, storing multiple pieces of information, and more advanced decision tools.
+
+## Topics Covered
+
+* Loops `for`, `while`, `for of`
+* Arrays
+
+## Assignment
+
+As always, start off by forking and cloning this repository from GitHub. Open the code up in Atom to get started. 
+
+> Note that in the future we will not call this out explicitly. We'll assume you already know that is how you start your work. 
+
+### Reminder - Levels of Difficulty
+
+- Required Features are required and focus on the key skills for the week
+- Stretch Goals are __*optional*__!
+  - These will stretch and challenge deeper understanding of the material.
+  - Note that these may require additional research beyond the provided content. 
+
+### Files Provided
+
+- *index.html* (Note: loops-practice.js is already sourced in)
+- *loops-practice.js* (Required Features, & Stretch Goals)
+
+In the file there are some examples of working with arrays followed by __TODO__ comments with tasks for you to complete by writing some code following up on what is shown in the example. 
+
+> Please do not remove the instruction comments as you complete your work. 
+
+
+## Assignment Submission
+Check in your repo, then turn in your work via the Prime Academy Assignment Application at http://primeacademy.io, as usual and don't hesitate to hit up the Slack channel as needed!
+
+**Make sure that you answer this week's discussion question in Slack!**
---
+++ assignment/scripts/loops-practice.js
@@ -0,0 +1,47 @@
+console.log('****** Loops Practice *******');
+
+
+// 1. 'for' loop
+console.log('---- 1. For loops ----');
+// Example: a for loop to console.log numbers from 0 to 3
+console.log('count from 0 to 3');
+// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
+for (let i=0; i<4; i++) {  
+  console.log(i);
+}
+
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+//   - Which part of the example loop do you need to change to do this?
+console.log('count from 0 to 5');
+
+// 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
+//   - Which part of the example loop do you need to change to do this?
+console.log('count from 3 to 5');
+
+// 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
+//   - Which part of the example loop do you need to change to do this?
+console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+
+// 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
+console.log('STRETCH: countdown from 5 to 0');
+
+
+// 2. For of loops  
+console.log('---- 2. For Of loop ----');
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+
+// 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
+console.log('Some stars:');
+
+
+// 3. While loops  
+console.log('---- 3. While loop ----');
+
+// 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
+console.log('Some stars using while:');
+
+// 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
+console.log('count from 0 to 5');
+
+// 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
+console.log('count backwards from 10 to 5');
