c7082620272dbf0b1c385412b8992a7e916e8b9f
Edan Schwartz <edanschwartz@gmail.com>
2022-02-09 11:05:30 -0600

    typo in instructions

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,7 +6,7 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i = 0; i < 4; i++) {
   console.log(i);
 }
 
@@ -22,7 +22,7 @@ console.log('count from 3 to 5');
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
-// 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
+// 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
 
