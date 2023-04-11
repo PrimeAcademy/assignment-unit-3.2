9ab451deb82322d5d6abafb796996d8b72177e74
Javier Navarro <javierna182@gmail.com>
2023-04-10 20:12:01 -0500

    final review

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -37,9 +37,7 @@ for (let i=3; i<6; i++) {
 
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
-let number=10;
-
-for (let i=1; i<=number; i++) {   
+for (let i=1; i<=10; i++) {   
 if (i%2==0)   
 {  
 console.log(i);  
